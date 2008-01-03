feeb78b45cd91ca862901630377b8c88ee9410fe
yamasaki <yamasaki@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-01-03 03:48:47 +0000

    新規作成用のウィザードを追加した（作成中）。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@120 eeba10ee-d406-0410-b51a-bfa74c1c70a0

---
+++ statsvision/src/org/seasar/javelin/statsvision/wizard/NewFileWizardPage.java
@@ -0,0 +1,16 @@
+package org.seasar.javelin.statsvision.wizard;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Composite;
+
+public class NewFileWizardPage extends WizardPage
+{
+    public NewFileWizardPage()
+    {
+        super("Create New StatsVision File");
+    }
+
+    public void createControl(Composite parent)
+    {
+    }
+}
