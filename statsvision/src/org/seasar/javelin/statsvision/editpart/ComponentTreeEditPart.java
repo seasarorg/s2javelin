54b3ab23caf423e6755c1ccef5ae6769147111db
yamasaki <yamasaki@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-01-03 11:20:29 +0000

    アウトライン機能を追加した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@127 eeba10ee-d406-0410-b51a-bfa74c1c70a0

---
+++ statsvision/src/org/seasar/javelin/statsvision/editpart/ComponentTreeEditPart.java
@@ -0,0 +1,21 @@
+package org.seasar.javelin.statsvision.editpart;
+
+import java.beans.PropertyChangeEvent;
+
+import org.seasar.javelin.statsvision.model.ComponentModel;
+
+public class ComponentTreeEditPart extends StatsVisionTreeEditPart
+{
+    // オーバーライド
+    protected void refreshVisuals() {
+      ComponentModel model = (ComponentModel)getModel();
+      // ツリー・アイテムのテキストとしてモデルのテキストを設定
+      setWidgetText(model.getClassName());
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+      // モデルのテキストの変更をツリー・アイテムにも反映させる
+      if (evt.getPropertyName().equals(ComponentModel.P_CLASS_NAME))
+        refreshVisuals();
+    }
+}
