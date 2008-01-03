95fb4e9aff16c91f3ff3a8a7f4bd4ddd89822743
yamasaki <yamasaki@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-01-03 09:33:08 +0000

    ファイル作成用のWizardを追加した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@125 eeba10ee-d406-0410-b51a-bfa74c1c70a0

--- statsvision/src/org/seasar/javelin/statsvision/wizard/NewFileWizard.java
+++ statsvision/src/org/seasar/javelin/statsvision/wizard/NewFileWizard.java
@@ -1,34 +1,59 @@
 package org.seasar.javelin.statsvision.wizard;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
 public class NewFileWizard extends Wizard implements INewWizard
 {
+    private static final String TITLE = "New StatsVision File";
 
+    private WizardNewFileCreationPage creationPage;
+
+    private IStructuredSelection selection;
+
+    private NewFileWizardPage modifyPage;
+    
     public NewFileWizard()
     {
-        // TODO Auto-generated constructor stub
+        setWindowTitle(TITLE);
     }
 
+    public void init(IWorkbench workbench, IStructuredSelection selection)
+    {
+        this.selection = selection;
+    }
+    
     public void addPages()
     {
-        addPage(new NewFileWizardPage());
+        creationPage = new WizardNewFileCreationPage("", selection);
+        creationPage.setTitle("StatsVision");
+        creationPage.setDescription("Create new StatsVision file.");
+        creationPage.setFileExtension("jview");
+
+        modifyPage = new NewFileWizardPage();
+
+        addPage(creationPage);
+        addPage(modifyPage);
     }
     
     @Override
     public boolean performFinish()
     {
-        // TODO Auto-generated method stub
+        IFile file = creationPage.createNewFile();
+        try
+        {
+            modifyPage.save(file);
+        }
+        catch (CoreException ex)
+        {
+            ex.printStackTrace();
+        }
+        
         return true;
     }
-
-    public void init(IWorkbench workbench, IStructuredSelection selection)
-    {
-        // TODO Auto-generated method stub
-
-    }
-
 }
