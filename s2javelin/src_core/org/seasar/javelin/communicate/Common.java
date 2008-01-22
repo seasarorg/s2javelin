eead3ee652dda4bc0125bd8966c66d4e7412b178
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-01-22 02:18:34 +0000

    定数にfinalを付加した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@195 eeba10ee-d406-0410-b51a-bfa74c1c70a0

--- s2javelin/src_core/org/seasar/javelin/communicate/Common.java
+++ s2javelin/src_core/org/seasar/javelin/communicate/Common.java
@@ -19,33 +19,33 @@ public class Common
 	/**
 	 * 電文種別(アラーム)
 	 */
-	public static byte BYTE_TELEGRAM_KIND_ALERT = 0;
+	public static final byte BYTE_TELEGRAM_KIND_ALERT = 0;
 
 	/**
 	 * 電文種別(状態取得)
 	 */
-	public static byte BYTE_TELEGRAM_KIND_GET = 1;
+	public static final byte BYTE_TELEGRAM_KIND_GET = 1;
 	
 	/**
 	 * 電文種別(リセット)
 	 */
-	public static byte BYTE_TELEGRAM_KIND_RESET = 2;
+	public static final byte BYTE_TELEGRAM_KIND_RESET = 2;
 
 	/**
 	 * 要求応答種別(通知)
 	 */
-	public static byte BYTE_REQUEST_KIND_NOTIFY = 0;
+	public static final byte BYTE_REQUEST_KIND_NOTIFY = 0;
 
 
 	/**
 	 * 要求応答種別(要求)
 	 */
-	public static byte BYTE_REQUEST_KIND_REQUEST = 1;
+	public static final byte BYTE_REQUEST_KIND_REQUEST = 1;
 
 	/**
 	 * 要求応答種別(応答)
 	 */
-	public static byte BYTE_REQUEST_KIND_RESPONSE = 2;
+	public static final byte BYTE_REQUEST_KIND_RESPONSE = 2;
 
 	
 	public static final byte BYTE_ITEMMODE_KIND_8BYTE_INT = 3;
