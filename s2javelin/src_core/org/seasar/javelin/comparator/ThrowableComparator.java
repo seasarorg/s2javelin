597068205d5037284455ba7faadf27b58458e582
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-01-22 01:48:00 +0000

    Genericsを使用するように修正した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@193 eeba10ee-d406-0410-b51a-bfa74c1c70a0

--- s2javelin/src_core/org/seasar/javelin/comparator/ThrowableComparator.java
+++ s2javelin/src_core/org/seasar/javelin/comparator/ThrowableComparator.java
@@ -3,17 +3,14 @@ package org.seasar.javelin.comparator;
 import java.io.Serializable;
 import java.util.Comparator;
 
-import org.seasar.javelin.bean.Invocation;
+import org.seasar.javelin.bean.InvocationMBean;
 
-public class ThrowableComparator implements Comparator, Serializable
+public class ThrowableComparator implements Comparator<InvocationMBean>, Serializable
 {
 	private static final long serialVersionUID = 2243227887834030002L;
 
-	public int compare(Object arg0, Object arg1)
+	public int compare(InvocationMBean invocation0, InvocationMBean invocation1)
 	{
-		Invocation invocation0 = (Invocation)arg0;
-		Invocation invocation1 = (Invocation)arg1;
-		
 		if (invocation0.getThrowableCount() > invocation1.getThrowableCount())
 		{
 			return -1;
