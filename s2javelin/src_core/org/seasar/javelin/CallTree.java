c4a757b6f69278efb190371d50f155532a186e26
yamasaki <yamasaki@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-02-09 06:05:23 +0000

    SQLトレース対応用のフラグ、コールバック機構を追加した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@212 eeba10ee-d406-0410-b51a-bfa74c1c70a0

--- s2javelin/src_core/org/seasar/javelin/CallTree.java
+++ s2javelin/src_core/org/seasar/javelin/CallTree.java
@@ -1,9 +1,15 @@
 package org.seasar.javelin;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.seasar.javelin.CallTreeNode;
 import org.seasar.javelin.util.StatsUtil;
 
 /**
+ * Javelinログ出力用にコールスタックを記録するための、ツリークラス。
  * 
  * @author yamasaki
  */
@@ -11,11 +17,15 @@ public class CallTree
 {
     private CallTreeNode rootNode_;
 
-    private String       threadID_;
+    private String threadID_;
 
-    private String		  rootCallerName_ = "unknown";
+    private String rootCallerName_ = "unknown";
     
-    private String 	  endCalleeName_ = "unknown";
+    private String endCalleeName_ = "unknown";
+
+    private List<Callback> callbackList_ = new ArrayList<Callback>();
+
+    private Set<Object> flagSet_ = new HashSet<Object>();
     
     public CallTree()
     {
@@ -64,5 +74,33 @@ public class CallTree
 		rootCallerName_ = rootCallerName;
 	}
 
-
+	public void addCallback(Callback callback)
+	{
+		callbackList_.add(callback);
+	}
+	
+	public void executeCallback()
+	{
+		for (Callback callback : callbackList_)
+		{
+			try
+			{
+				callback.execute();
+			}
+			catch(Exception ex)
+			{
+				JavelinErrorLogger.getInstance().log(ex);
+			}
+		}
+	}
+	
+	public boolean set(Object flag)
+	{
+		return flagSet_.add(flag);
+	}
+	
+	public boolean contains(Object flag)
+	{
+		return flagSet_.contains(flag);
+	}
 }
---
+++ s2javelin/src_core/org/seasar/javelin/Callback.java
@@ -0,0 +1,6 @@
+package org.seasar.javelin;
+
+public interface Callback
+{
+	void execute();
+}
