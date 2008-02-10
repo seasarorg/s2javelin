1da3a848ada995f23d0f2ec78fc1b38c49a40d2e
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-02-10 11:26:03 +0000

    ファイルI/Oのユーティリティ
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@239 eeba10ee-d406-0410-b51a-bfa74c1c70a0

---
+++ s2javelin/src_core/org/seasar/javelin/util/IOUtil.java
@@ -0,0 +1,69 @@
+package org.seasar.javelin.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.seasar.javelin.JavelinErrorLogger;
+
+public class IOUtil {
+	private static final int BUFFER_SIZE = 1024;
+
+	public static long copy(InputStream input, OutputStream output)
+			throws IOException {
+		byte[] buffer = new byte[BUFFER_SIZE];
+		long count = 0;
+		int n = 0;
+		while (-1 != (n = input.read(buffer))) {
+			output.write(buffer, 0, n);
+			count += n;
+		}
+		return count;
+	}
+
+	public static long copy(Reader input, Writer output)
+			throws IOException {
+		char[] buffer = new char[BUFFER_SIZE];
+		long count = 0;
+		int n = 0;
+		while (-1 != (n = input.read(buffer))) {
+			output.write(buffer, 0, n);
+			count += n;
+		}
+		return count;
+	}
+
+	public static String readFileToString(String jvnFileName) {
+		String content = "";
+		Reader input = null;
+		StringWriter output = null;
+		try {
+			input = new FileReader(jvnFileName);
+			output = new StringWriter();
+			copy(input, output);
+		} catch (FileNotFoundException fnfe) {
+			JavelinErrorLogger.getInstance().log(fnfe);
+		} catch (IOException ioe) {
+			JavelinErrorLogger.getInstance().log(ioe);
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException ioe) {
+					JavelinErrorLogger.getInstance().log(ioe);
+				}
+			}
+		}
+	
+		if (output != null) {
+			content = output.toString();
+		}
+		return content;
+	}
+
+}
