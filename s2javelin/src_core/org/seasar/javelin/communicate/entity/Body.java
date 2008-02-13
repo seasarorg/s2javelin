8146d88e9dda85aacfb01846dc46f4a7e4862a8b
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-02-13 14:08:27 +0000

    ResponseBodyの内容をBodyに移動した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@259 eeba10ee-d406-0410-b51a-bfa74c1c70a0

--- s2javelin/src_core/org/seasar/javelin/communicate/entity/Body.java
+++ s2javelin/src_core/org/seasar/javelin/communicate/entity/Body.java
@@ -15,6 +15,21 @@ public class Body
      */
 	private String strItemName_ = "";
 	
+    /**
+     * €–ÚŒ^
+     */
+    private byte byteItemMode_ = 0;
+
+    /**
+     * ŒJ‚è•Ô‚µ‰ñ”
+     */
+    private int intLoopCount_ = 0;
+
+    /**
+     * à–¾
+     */
+    private Object[] objItemValueArr_ = null;
+    
 	/**
      * ƒIƒuƒWƒFƒNƒg–¼æ“¾
      */
@@ -42,4 +57,46 @@ public class Body
 	public void setStrItemName(String strItemName_) {
 		this.strItemName_ = strItemName_;
 	}
+
+    /**
+     * €–ÚŒ^æ“¾
+     */
+    public byte getByteItemMode() {
+        return byteItemMode_;
+    }
+
+    /**
+     * €–ÚŒ^İ’è
+     */
+    public void setByteItemMode(byte byteItemMode_) {
+        this.byteItemMode_ = byteItemMode_;
+    }
+
+    /**
+     * ŒJ‚è•Ô‚µ‰ñ”æ“¾
+     */
+    public int getIntLoopCount() {
+        return intLoopCount_;
+    }
+
+    /**
+     * ŒJ‚è•Ô‚µ‰ñ”İ’è
+     */
+    public void setIntLoopCount(int intLoopCount_) {
+        this.intLoopCount_ = intLoopCount_;
+    }
+
+    /**
+     * à–¾æ“¾
+     */
+    public Object[] getObjItemValueArr() {
+        return objItemValueArr_;
+    }
+
+    /**
+     * à–¾İ’è
+     */
+    public void setObjItemValueArr(Object[] objItemValueArr_) {
+        this.objItemValueArr_ = objItemValueArr_;
+    }
 }
