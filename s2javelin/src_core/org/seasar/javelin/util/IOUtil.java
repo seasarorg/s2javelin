2674a5ebae8daa01523bb193a2c6c9d1609d2b24
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-02-15 03:48:55 +0000

    コピーするファイルのサイズ指定処理を修正した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@263 eeba10ee-d406-0410-b51a-bfa74c1c70a0

--- s2javelin/src_core/org/seasar/javelin/util/IOUtil.java
+++ s2javelin/src_core/org/seasar/javelin/util/IOUtil.java
@@ -26,26 +26,41 @@ public class IOUtil {
 		return count;
 	}
 
-	public static long copy(Reader input, Writer output)
+    public static long copy(Reader input, Writer output)
+        throws IOException
+    {
+        return copy(input, output, -1);
+    }
+
+    public static long copy(Reader input, Writer output, int maxBytes)
 			throws IOException {
 		char[] buffer = new char[BUFFER_SIZE];
 		long count = 0;
 		int n = 0;
-		while (-1 != (n = input.read(buffer))) {
+		while (count < maxBytes && (-1 != (n = input.read(buffer)))) {
+		    if(count + n > maxBytes)
+		    {
+		        n = (int)(maxBytes - count);
+		    }
+		    
 			output.write(buffer, 0, n);
 			count += n;
 		}
 		return count;
 	}
 
-	public static String readFileToString(String jvnFileName) {
+    public static String readFileToString(String jvnFileName) {
+        return readFileToString(jvnFileName, -1);
+    }
+    
+    public static String readFileToString(String jvnFileName, int maxBytes) {
 		String content = "";
 		Reader input = null;
 		StringWriter output = null;
 		try {
 			input = new FileReader(jvnFileName);
 			output = new StringWriter();
-			copy(input, output);
+			copy(input, output, maxBytes);
 		} catch (FileNotFoundException fnfe) {
 			JavelinErrorLogger.getInstance().log(fnfe);
 		} catch (IOException ioe) {
