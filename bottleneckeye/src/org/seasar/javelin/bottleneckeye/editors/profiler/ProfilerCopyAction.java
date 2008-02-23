6a886413b82b48acf76730bf2250a7d0f1b69ad3
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-02-23 14:28:14 +0000

    プロファイラビューを追加した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@270 eeba10ee-d406-0410-b51a-bfa74c1c70a0

---
+++ bottleneckeye/src/org/seasar/javelin/bottleneckeye/editors/profiler/ProfilerCopyAction.java
@@ -0,0 +1,142 @@
+package org.seasar.javelin.bottleneckeye.editors.profiler;
+
+import static org.seasar.javelin.bottleneckeye.editors.profiler.ProfilerTab.AVERAGE_TIME;
+import static org.seasar.javelin.bottleneckeye.editors.profiler.ProfilerTab.CALL_TIME;
+import static org.seasar.javelin.bottleneckeye.editors.profiler.ProfilerTab.CLASS_NAME;
+import static org.seasar.javelin.bottleneckeye.editors.profiler.ProfilerTab.MAX_TIME;
+import static org.seasar.javelin.bottleneckeye.editors.profiler.ProfilerTab.METHOD_NAME;
+import static org.seasar.javelin.bottleneckeye.editors.profiler.ProfilerTab.MIN_TIME;
+import static org.seasar.javelin.bottleneckeye.editors.profiler.ProfilerTab.THROWABLE_TIME;
+import static org.seasar.javelin.bottleneckeye.editors.profiler.ProfilerTab.TOTAL_TIME;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.TableColumn;
+import org.seasar.javelin.bottleneckeye.model.InvocationModel;
+
+/**
+ * プロファイラビューでコピーを行った際のアクション。
+ * @author cero-t
+ *
+ */
+public class ProfilerCopyAction extends Action
+{
+    /** 改行記号 */
+    private static final String NEWLINE = "\n";
+
+    /** クリップボードにコピーする際のヘッダ */
+    private static final String HEADER;
+
+    /** クリップボード */
+    private static Clipboard    clipboard__;
+
+    /** コピー対象のTableViewer */
+    private TableViewer         viewer_;
+
+    static
+    {
+        String[] headers = {CLASS_NAME, METHOD_NAME, TOTAL_TIME, AVERAGE_TIME, MAX_TIME, MIN_TIME,
+            CALL_TIME, THROWABLE_TIME};
+        HEADER = StringUtils.join(headers, "\t") + NEWLINE;
+    }
+
+    /**
+     * コンストラクタ。TableViewerを設定する。
+     * @param viewer TableViewer
+     */
+    public ProfilerCopyAction(TableViewer viewer)
+    {
+        this.viewer_ = viewer;
+    }
+
+    /**
+     * {@inheritDoc}<br>
+     * テーブルの内容をクリップボードにコピーする。<br>
+     * クリップボードには、各要素がタブ結合され、さらに各行を改行で結合した文字列がコピーされる。<br>
+     * なお、テーブル表示用文字列は、ColumnLabelProviderを用いて、<br>
+     * InvocationModelから取得する。
+     */
+    @Override
+    public void run()
+    {
+        TextTransfer textTransfer = TextTransfer.getInstance();
+        Object[] selection = ((IStructuredSelection)this.viewer_.getSelection()).toArray();
+
+        ColumnLabelProvider[] providers = getProviders();
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(HEADER);
+        for (int index = 0; index < selection.length; index++)
+        {
+            Object object = selection[index];
+            if (object instanceof InvocationModel == false)
+            {
+                continue;
+            }
+
+            String str = createSingleLine(providers, (InvocationModel)object);
+
+            builder.append("\"").append(str).append("\"");
+            builder.append(NEWLINE);
+        }
+        String content = new String(builder);
+
+        getClipboard().setContents(new Object[]{content}, new Transfer[]{textTransfer});
+    }
+
+    /**
+     * テーブルより、ColumnLabelProviderの配列を取得する。
+     * @return ColumnLabelProviderの配列
+     */
+    protected ColumnLabelProvider[] getProviders()
+    {
+        TableColumn[] columns = this.viewer_.getTable().getColumns();
+        int columnSize = columns.length;
+
+        ColumnLabelProvider[] providers = new ColumnLabelProvider[columnSize];
+        for (int index = 0; index < columnSize; index++)
+        {
+            providers[index] = (ColumnLabelProvider)this.viewer_.getLabelProvider(index);
+        }
+
+        return providers;
+    }
+
+    /**
+     * InvocationModelから、1行の文字列を作成する。
+     * @param providers ColumnLabelProviderの配列
+     * @param model InvocationModel
+     * @return 1行の文字列
+     */
+    protected String createSingleLine(ColumnLabelProvider[] providers, InvocationModel model)
+    {
+        String[] labels = new String[providers.length];
+        for (int columnIndex = 0; columnIndex < providers.length; columnIndex++)
+        {
+            labels[columnIndex] = providers[columnIndex].getText(model);
+        }
+        String str = StringUtils.join(labels, "\"\t\"");
+        return str;
+    }
+
+    /**
+     * クリップボードを取得する。
+     * @return クリップボード
+     */
+    protected static Clipboard getClipboard()
+    {
+        if (clipboard__ == null)
+        {
+            clipboard__ = new Clipboard(Display.getCurrent());
+        }
+        return clipboard__;
+    }
+
+}
---
+++ bottleneckeye/src/org/seasar/javelin/bottleneckeye/editors/profiler/ProfilerTab.java
@@ -0,0 +1,359 @@
+package org.seasar.javelin.bottleneckeye.editors.profiler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.CellEditorActionHandler;
+import org.eclipse.ui.part.MultiPageEditorPart;
+import org.seasar.javelin.bottleneckeye.StatsVisionPlugin;
+import org.seasar.javelin.bottleneckeye.communicate.Telegram;
+import org.seasar.javelin.bottleneckeye.editors.EditorTabInterface;
+import org.seasar.javelin.bottleneckeye.event.DataChangeListener;
+import org.seasar.javelin.bottleneckeye.model.InvocationModel;
+import org.seasar.javelin.bottleneckeye.model.MainCtrl;
+import org.seasar.javelin.bottleneckeye.views.ColumnViewerSorter;
+
+/**
+ * プロファイラ。
+ * @author cero-t
+ */
+public class ProfilerTab implements EditorTabInterface, DataChangeListener
+{
+    /** クラス名 */
+    protected static final String        CLASS_NAME     = "クラス名";
+
+    /** メソッド名 */
+    protected static final String        METHOD_NAME    = "メソッド名";
+
+    /** 合計処理時間 */
+    protected static final String        TOTAL_TIME     = "合計処理時間";
+
+    /** 平均処理時間 */
+    protected static final String        AVERAGE_TIME   = "平均処理時間";
+
+    /** 最大処理時間 */
+    protected static final String        MAX_TIME       = "最大処理時間";
+
+    /** 最小処理時間 */
+    protected static final String        MIN_TIME       = "最小処理時間";
+
+    /** 呼び出し回数 */
+    protected static final String        CALL_TIME      = "呼び出し回数";
+
+    /** 例外発生回数 */
+    protected static final String        THROWABLE_TIME = "例外発生回数";
+
+    /** テーブルビューワ。 */
+    private TableViewer                  viewer_;
+
+    /** テーブルに表示するデータの元となるマップ */
+    private Map<String, InvocationModel> modelMap_      = new HashMap<String, InvocationModel>();
+
+    /** テーブルに表示するデータのリスト */
+    private List<InvocationModel>        modelList_     = new ArrayList<InvocationModel>();
+
+    /**
+     * {@inheritDoc}
+     */
+    public Composite createComposite(Composite container, MultiPageEditorPart editorPart)
+    {
+        Composite composite = new Composite(container, SWT.NONE);
+        GridLayout layout = new GridLayout(1, false);
+        composite.setLayout(layout);
+
+        Button button = new Button(composite, SWT.PUSH | SWT.FLAT);
+        Image image = StatsVisionPlugin.getDefault().getImageRegistry().get(
+                                                                            StatsVisionPlugin.IMG_REFRESH);
+        button.setImage(image);
+        button.setLayoutData(new GridData());
+        button.addSelectionListener(new SelectionListener() {
+            public void widgetDefaultSelected(SelectionEvent e)
+            {
+                // Do Nothing.
+            }
+
+            public void widgetSelected(SelectionEvent e)
+            {
+                ProfilerTab.this.modelList_.clear();
+                ProfilerTab.this.modelList_.addAll(ProfilerTab.this.modelMap_.values());
+                ProfilerTab.this.viewer_.refresh(true);
+            }
+        });
+
+        this.viewer_ = new TableViewer(composite, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI);
+        this.viewer_.setContentProvider(new ArrayContentProvider());
+        this.viewer_.getTable().setLinesVisible(true);
+        this.viewer_.getTable().setHeaderVisible(true);
+        this.viewer_.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+
+        createColumns(this.viewer_);
+
+        this.viewer_.setInput(this.modelList_);
+        MainCtrl.getInstance().addDataChangeListeners(this);
+
+        CellEditorActionHandler actionHandler = new CellEditorActionHandler(
+                                                                            editorPart.getEditorSite().getActionBars());
+        ProfilerCopyAction action = new ProfilerCopyAction(this.viewer_);
+        actionHandler.setCopyAction(action);
+
+        return composite;
+    }
+
+    /**
+     * カラムを作成する。
+     * @param viewer TableViewer
+     */
+    private void createColumns(TableViewer viewer)
+    {
+        TableViewerColumn column = new TableViewerColumn(viewer, SWT.LEFT);
+        column.getColumn().setText(CLASS_NAME);
+        column.getColumn().setWidth(200);
+        column.getColumn().setMoveable(true);
+        column.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element)
+            {
+                return ((InvocationModel)element).getClassName();
+            }
+        });
+        new ColumnViewerSorter(viewer, column) {
+            @Override
+            protected int doCompare(Object e1, Object e2)
+            {
+                String s1 = ((InvocationModel)e1).getClassName();
+                String s2 = ((InvocationModel)e2).getClassName();
+                return s1.compareToIgnoreCase(s2);
+            }
+        };
+
+        column = new TableViewerColumn(viewer, SWT.LEFT);
+        column.getColumn().setText(METHOD_NAME);
+        column.getColumn().setWidth(150);
+        column.getColumn().setMoveable(true);
+        column.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element)
+            {
+                return ((InvocationModel)element).getMethodName();
+            }
+        });
+        new ColumnViewerSorter(viewer, column) {
+            @Override
+            protected int doCompare(Object e1, Object e2)
+            {
+                String s1 = ((InvocationModel)e1).getMethodName();
+                String s2 = ((InvocationModel)e2).getMethodName();
+                return s1.compareToIgnoreCase(s2);
+            }
+        };
+
+        column = new TableViewerColumn(viewer, SWT.LEFT);
+        column.getColumn().setText(TOTAL_TIME);
+        column.getColumn().setWidth(100);
+        column.getColumn().setMoveable(true);
+        column.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element)
+            {
+                long count = ((InvocationModel)element).getCount();
+                long average = ((InvocationModel)element).getAverage();
+                long total = count * average;
+                return String.valueOf(total);
+            }
+        });
+        new ColumnViewerSorter(viewer, column) {
+            @Override
+            protected int doCompare(Object e1, Object e2)
+            {
+                long l1 = ((InvocationModel)e1).getCount() * ((InvocationModel)e1).getAverage();
+                long l2 = ((InvocationModel)e2).getCount() * ((InvocationModel)e2).getAverage();
+                return (l1 < l2 ? -1 : (l1 == l2 ? 0 : 1));
+            }
+        };
+
+        column = new TableViewerColumn(viewer, SWT.LEFT);
+        column.getColumn().setText(AVERAGE_TIME);
+        column.getColumn().setWidth(100);
+        column.getColumn().setMoveable(true);
+        column.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element)
+            {
+                return String.valueOf(((InvocationModel)element).getAverage());
+            }
+        });
+        new ColumnViewerSorter(viewer, column) {
+            @Override
+            protected int doCompare(Object e1, Object e2)
+            {
+                long l1 = ((InvocationModel)e1).getAverage();
+                long l2 = ((InvocationModel)e2).getAverage();
+                return (l1 < l2 ? -1 : (l1 == l2 ? 0 : 1));
+            }
+        };
+
+        column = new TableViewerColumn(viewer, SWT.LEFT);
+        column.getColumn().setText(MAX_TIME);
+        column.getColumn().setWidth(100);
+        column.getColumn().setMoveable(true);
+        column.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element)
+            {
+                return String.valueOf(((InvocationModel)element).getMaximum());
+            }
+        });
+        new ColumnViewerSorter(viewer, column) {
+            @Override
+            protected int doCompare(Object e1, Object e2)
+            {
+                long l1 = ((InvocationModel)e1).getMaximum();
+                long l2 = ((InvocationModel)e2).getMaximum();
+                return (l1 < l2 ? -1 : (l1 == l2 ? 0 : 1));
+            }
+        };
+
+        column = new TableViewerColumn(viewer, SWT.LEFT);
+        column.getColumn().setText(MIN_TIME);
+        column.getColumn().setWidth(100);
+        column.getColumn().setMoveable(true);
+        column.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element)
+            {
+                return String.valueOf(((InvocationModel)element).getMinimum());
+            }
+        });
+        new ColumnViewerSorter(viewer, column) {
+            @Override
+            protected int doCompare(Object e1, Object e2)
+            {
+                long l1 = ((InvocationModel)e1).getMinimum();
+                long l2 = ((InvocationModel)e2).getMinimum();
+                return (l1 < l2 ? -1 : (l1 == l2 ? 0 : 1));
+            }
+        };
+        column = new TableViewerColumn(viewer, SWT.LEFT);
+        column.getColumn().setText(CALL_TIME);
+        column.getColumn().setWidth(100);
+        column.getColumn().setMoveable(true);
+        column.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element)
+            {
+                return String.valueOf(((InvocationModel)element).getCount());
+            }
+        });
+        new ColumnViewerSorter(viewer, column) {
+            @Override
+            protected int doCompare(Object e1, Object e2)
+            {
+                long l1 = ((InvocationModel)e1).getCount();
+                long l2 = ((InvocationModel)e2).getCount();
+                return (l1 < l2 ? -1 : (l1 == l2 ? 0 : 1));
+            }
+        };
+
+        column = new TableViewerColumn(viewer, SWT.LEFT);
+        column.getColumn().setText(THROWABLE_TIME);
+        column.getColumn().setWidth(100);
+        column.getColumn().setMoveable(true);
+        column.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element)
+            {
+                return String.valueOf(((InvocationModel)element).getThrowableCount());
+            }
+        });
+        new ColumnViewerSorter(viewer, column) {
+            @Override
+            protected int doCompare(Object e1, Object e2)
+            {
+                long l1 = ((InvocationModel)e1).getThrowableCount();
+                long l2 = ((InvocationModel)e2).getThrowableCount();
+                return (l1 < l2 ? -1 : (l1 == l2 ? 0 : 1));
+            }
+        };
+    }
+
+    /**
+     * ビューの更新を行う。
+     */
+    public void refresh()
+    {
+        this.viewer_.refresh();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getName()
+    {
+        return "Profiler";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean receiveTelegram(Telegram telegram)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onCopy()
+    {
+        // Do Nothing.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onPrint()
+    {
+        // Do Nothing.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onReset()
+    {
+        // Do Nothing.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onStart()
+    {
+        this.modelMap_.clear();
+        this.viewer_.getTable().clearAll();
+        this.viewer_.refresh(true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onStop()
+    {
+        // Do Nothing.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void updateData(Object element)
+    {
+        InvocationModel model = (InvocationModel)element;
+        this.modelMap_.put(model.getClassName() + "#" + model.getMethodName(), model);
+    }
+}
