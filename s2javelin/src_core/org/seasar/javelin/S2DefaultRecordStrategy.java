ec5d13074e350c29211243e4cf2b7fc17aa6666f
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-04-12 14:27:38 +0000

    クラス名をDefaultRecordStrategyから、S2DefaultRecordStrategyに変更。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@377 eeba10ee-d406-0410-b51a-bfa74c1c70a0

---
+++ s2javelin/src_core/org/seasar/javelin/S2DefaultRecordStrategy.java
@@ -0,0 +1,52 @@
+package org.seasar.javelin;
+
+import org.seasar.javelin.log.JavelinLogCallback;
+
+/**
+ * TATの値が設定した閾値を越えている場合、ログファイル出力、アラーム通知を行う。
+ * @author eriguchi
+ *
+ */
+public class S2DefaultRecordStrategy implements RecordStrategy
+{
+    /**
+     * Javelinログをファイルに出力かどうか判定する</br>
+     * AccumulatedTimeがjavelin.recordThresholdに設定した値以上のときに出力する。
+     * @param node CallTreeNode
+     * @return true:ログファイル出力を行う、false：ログファイル出力を行わない。
+     */
+    public boolean judgeGenerateJaveinFile(CallTreeNode node)
+    {
+        if (node.getAccumulatedTime() > node.getInvocation().getRecordThreshold())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * アラームを通知するかどうか判定する</br>
+     * AccumulatedTimeがjavelin.alarmThresholdに設定した値以上のときに出力する。
+     * @param node CallTreeNode
+     * @return true:アラーム通知を行う、false：アラーム通知を行わない。
+     */
+    public boolean judgeSendExceedThresholdAlarm(CallTreeNode node)
+    {
+        if (node.getAccumulatedTime() >= node.getInvocation().getAlarmThreshold())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 何もしない。
+     * @param node CallTreeNode
+     * @return null
+     */
+    public JavelinLogCallback createCallback(CallTreeNode node)
+    {
+        // Do Nothing
+        return null;
+    }
+}
