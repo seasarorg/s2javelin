1565fc988511320050d06ee8926c5d317c44e4a7
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-04-14 11:00:44 +0000

    コメントを追加した。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@385 eeba10ee-d406-0410-b51a-bfa74c1c70a0

--- s2javelin/src_core/org/seasar/javelin/CallTreeNode.java
+++ s2javelin/src_core/org/seasar/javelin/CallTreeNode.java
@@ -2,234 +2,401 @@ package org.seasar.javelin;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.seasar.javelin.bean.Invocation;
 
-public class CallTreeNode {
-	private Invocation invocation_;
+/**
+ * メソッド呼び出し情報
+ * @author eriguchi
+ *
+ */
+public class CallTreeNode
+{
+    /**  */
+    private Invocation          invocation_;
 
-	private String returnValue_;
+    /** 戻り値 */
+    private String              returnValue_;
 
-	private Throwable throwable_;
+    /** 例外 */
+    private Throwable           throwable_;
 
-	private long throwTime_;
+    /** 例外発生回数 */
+    private long                throwTime_;
 
-	private long startTime_;
+    /** 開始時刻 */
+    private long                startTime_;
 
-	private long endTime_;
+    /** 終了時刻 */
+    private long                endTime_;
 
-	private long accumulatedTime_;
-	
-	private long cpuTime_;
+    /** 累積時間 */
+    private long                accumulatedTime_;
 
-    private long userTime_;
-	
-	private VMStatus startVmStatus_;
+    /** CPU時間 */
+    private long                cpuTime_;
 
-    private VMStatus endVmStatus_;
+    /** ユーザ時間 */
+    private long                userTime_;
 
-	private String[] args_;
+    /** 開始時のVMのステータス */
+    private VMStatus            startVmStatus_;
 
-	private StackTraceElement[] stacktrace_;
+    /** 終了時のVMのステータス */
+    private VMStatus            endVmStatus_;
 
-	private CallTreeNode parent_;
+    /** 引数 */
+    private String[]            args_;
 
-	private List<CallTreeNode> children_ = new ArrayList<CallTreeNode>();
+    /** スタックトレース */
+    private StackTraceElement[] stacktrace_;
 
-	private boolean isFieldAccess_;
+    /** CallTreeNodeの親ノード */
+    private CallTreeNode        parent_;
 
-	public Invocation getInvocation() {
-		return invocation_;
-	}
+    /** CallTreeNodeの子ノード */
+    private List<CallTreeNode>  children_ = new ArrayList<CallTreeNode>();
 
-	public void setInvocation(Invocation invocation) {
-		invocation_ = invocation;
-	}
+    /** フィールドアクセス */
+    private boolean             isFieldAccess_;
 
-	public String getReturnValue() {
-		return returnValue_;
-	}
+    /**
+     * InvoCation
+     * invocationを取得する。
+     * @return Invocation
+     */
+    public Invocation getInvocation()
+    {
+        return this.invocation_;
+    }
 
-	public void setReturnValue(String returnValue) {
-		returnValue_ = returnValue;
-	}
+    /**
+     * Invocationを設定する。
+     * @param invocation Invocation
+     */
+    public void setInvocation(Invocation invocation)
+    {
+        this.invocation_ = invocation;
+    }
 
-	public long getStartTime() {
-		return startTime_;
-	}
+    /**
+     * 戻り値を取得する。
+     * @return 戻り値
+     */
+    public String getReturnValue()
+    {
+        return this.returnValue_;
+    }
 
-	public void setStartTime(long startTime) {
-		startTime_ = startTime;
-	}
+    /**
+     * 戻り値を設定する。
+     * @param returnValue 戻り値
+     */
+    public void setReturnValue(String returnValue)
+    {
+        this.returnValue_ = returnValue;
+    }
 
-	public long getEndTime() {
-		return endTime_;
-	}
+    /**
+     * メソッド開始時刻を取得する。
+     * @return メソッド開始時刻
+     */
+    public long getStartTime()
+    {
+        return this.startTime_;
+    }
 
-	public void setEndTime(long endTime) {
-		endTime_ = endTime;
-		accumulatedTime_ = endTime_ - startTime_;
-		invocation_.setAccumulatedTime(accumulatedTime_);
-	}
+    /**
+     * メソッド開始時刻を取得する。
+     * @param startTime メソッド開始時刻
+     */
+    public void setStartTime(long startTime)
+    {
+        this.startTime_ = startTime;
+    }
 
-	public long getAccumulatedTime() {
-		return accumulatedTime_;
-	}
+    /**
+     * メソッドの終了時刻を取得する。
+     * @return メソッドの終了時刻
+     */
+    public long getEndTime()
+    {
+        return this.endTime_;
+    }
 
-	public void setCpuTime(long cpuTime)
-	{
-	    cpuTime_ = cpuTime;
-	}
+    /**
+     * メソッドの終了時刻を設定する。
+     * @param endTime メソッドの終了時刻
+     */
+    public void setEndTime(long endTime)
+    {
+        this.endTime_ = endTime;
+        this.accumulatedTime_ = this.endTime_ - this.startTime_;
+        this.invocation_.setAccumulatedTime(this.accumulatedTime_);
+    }
 
-	public long getCpuTime()
-	{
-	    return cpuTime_;
-	}
-	
-	public StackTraceElement[] getStacktrace() {
-		return stacktrace_;
-	}
+    /**
+     * 累積時間を取得する。
+     * @return 累積時間
+     */
+    public long getAccumulatedTime()
+    {
+        return this.accumulatedTime_;
+    }
 
-	public void setStacktrace(StackTraceElement[] stacktrace) {
-		stacktrace_ = stacktrace;
-	}
+    /**
+     * CPU時間を取得す。る
+     * @param cpuTime CPU時間
+     */
+    public void setCpuTime(long cpuTime)
+    {
+        this.cpuTime_ = cpuTime;
+    }
 
-	public CallTreeNode getParent() {
-		return parent_;
-	}
+    /**
+     * CPU時間を取得する。
+     * @return CPU時間
+     */
+    public long getCpuTime()
+    {
+        return this.cpuTime_;
+    }
 
-	public void setParent(CallTreeNode parent) {
-		parent_ = parent;
-	}
+    /**
+     * StackTraceを取得する。
+     * @return StackTrace
+     */
+    public StackTraceElement[] getStacktrace()
+    {
+        return this.stacktrace_;
+    }
 
-	public List<CallTreeNode> getChildren() {
-		return children_;
-	}
+    /**
+     * StackTraceを設定する。
+     * @param stacktrace StackTrace
+     */
+    public void setStacktrace(StackTraceElement[] stacktrace)
+    {
+        this.stacktrace_ = stacktrace;
+    }
 
-	public void addChild(CallTreeNode node) {
-		children_.add(node);
-		node.setParent(this);
-	}
+    /**
+     * CallTreeNodeの親を取得する。
+     * @return CallTreeNodeの親
+     */
+    public CallTreeNode getParent()
+    {
+        return this.parent_;
+    }
 
-	public String[] getArgs() {
-		return args_;
-	}
+    /**
+     * CallTreeNodeの親を設定する。
+     * @param parent CallTreeNodeの親
+     */
+    public void setParent(CallTreeNode parent)
+    {
+        this.parent_ = parent;
+    }
 
-	public void setArgs(String[] args) {
-		args_ = args;
-	}
+    /**
+     * CallTreeNOdeの子を取得する。
+     * @return CallTreeNodeの子
+     */
+    public List<CallTreeNode> getChildren()
+    {
+        return this.children_;
+    }
 
-	/**
-	 * このノードの親ノードを削除する。 親ノードがない場合は何もしない。
-	 * 
-	 * @param tree
-	 *            ツリー
-	 */
-	public void removeParent(CallTree tree) {
-		CallTreeNode parent = getParent();
-		if (parent != null) {
-			// 親の親があれば、親の親の中に自分を入れる。
-			// 親の親がなければ、自分がツリーのルートになる。
-			CallTreeNode grandParent = parent.getParent();
-			if (grandParent != null) {
-				int childIndex = grandParent.getChildIndex(parent);
-				if (childIndex != -1) {
-					grandParent.children_.set(childIndex, this);
-					setParent(grandParent);
-				}
-			} else {
-				tree.setRootNode(this);
-				setParent(null);
-			}
-		}
-	}
+    /**
+     * CallTreeNOdeの子を設定する。
+     * @param node CallTreeNOdeの子
+     */
+    public void addChild(CallTreeNode node)
+    {
+        this.children_.add(node);
+        node.setParent(this);
+    }
 
-	/**
-	 * このノードをツリーから削除する。
-	 * 
-	 * @return このノードの親
-	 */
-	public CallTreeNode remove() {
-		CallTreeNode parent = getParent();
-		if (parent != null) {
-			parent.children_.remove(this);
-		}
-		return parent;
-	}
+    /**
+     * 引数を取得する。
+     * @return 引数
+     */
+    public String[] getArgs()
+    {
+        return this.args_;
+    }
 
-	/**
-	 * 指定されたノードが何番目の子ノードかを調べる。
-	 * 
-	 * @param node
-	 *            ノード
-	 * @return ノードの番号。子ノードでなければ -1
-	 */
-	private int getChildIndex(CallTreeNode node) {
-		return this.children_.indexOf(node);
-	}
+    /**
+     * 引数を設定する。
+     * @param args 引数
+     */
+    public void setArgs(String[] args)
+    {
+        this.args_ = args;
+    }
 
-	/**
-	 * ノードがフィールドへのアクセスかどうかを示すフラグを取得する。
-	 * 
-	 * @return フィールドアクセスならtrue、そうでなければfalseを返す。
-	 */
-	public boolean isFieldAccess() {
-		return isFieldAccess_;
-	}
+    /**
+     * このノードの親ノードを削除する。 親ノードがない場合は何もしない。
+     * 
+     * @param tree ツリー
+     */
+    public void removeParent(CallTree tree)
+    {
+        CallTreeNode parent = getParent();
+        if (parent != null)
+        {
+            // 親の親があれば、親の親の中に自分を入れる。
+            // 親の親がなければ、自分がツリーのルートになる。
+            CallTreeNode grandParent = parent.getParent();
+            if (grandParent != null)
+            {
+                int childIndex = grandParent.getChildIndex(parent);
+                if (childIndex != -1)
+                {
+                    grandParent.children_.set(childIndex, this);
+                    setParent(grandParent);
+                }
+            }
+            else
+            {
+                tree.setRootNode(this);
+                setParent(null);
+            }
+        }
+    }
 
-	/**
-	 * ノードがフィールドへのアクセスかどうかを示すフラグを取得する。
-	 * 
-	 * @param isFieldAccess
-	 *            フィールドアクセスならtrue、そうでなければfalse。
-	 */
-	public void setFieldAccess(boolean isFieldAccess) {
-		this.isFieldAccess_ = isFieldAccess;
-	}
+    /**
+     * このノードをツリーから削除する。
+     * 
+     * @return このノードの親
+     */
+    public CallTreeNode remove()
+    {
+        CallTreeNode parent = getParent();
+        if (parent != null)
+        {
+            parent.children_.remove(this);
+        }
+        return parent;
+    }
 
-	public Throwable getThrowable() {
-		return throwable_;
-	}
+    /**
+     * 指定されたノードが何番目の子ノードかを調べる。
+     * 
+     * @param node ノード
+     * @return ノードの番号。子ノードでなければ -1
+     */
+    private int getChildIndex(CallTreeNode node)
+    {
+        return this.children_.indexOf(node);
+    }
 
-	public void setThrowable(Throwable throwable) {
-		throwable_ = throwable;
-	}
+    /**
+     * ノードがフィールドへのアクセスかどうかを示すフラグを取得する。
+     * 
+     * @return フィールドアクセスならtrue、そうでなければfalseを返す。
+     */
+    public boolean isFieldAccess()
+    {
+        return this.isFieldAccess_;
+    }
 
-	public long getThrowTime() {
-		return throwTime_;
-	}
+    /**
+     * ノードがフィールドへのアクセスかどうかを示すフラグを取得する。
+     * 
+     * @param isFieldAccess フィールドアクセスならtrue、そうでなければfalse。
+     */
+    public void setFieldAccess(boolean isFieldAccess)
+    {
+        this.isFieldAccess_ = isFieldAccess;
+    }
 
-	public void setThrowTime(long throwTime) {
-		throwTime_ = throwTime;
-	}
+    /**
+     * 例外を取得する。
+     * @return 例外
+     */
+    public Throwable getThrowable()
+    {
+        return this.throwable_;
+    }
 
-    public VMStatus getEndVmStatus( )
+    /**
+     * 例外を設定する。
+     * @param throwable 例外
+     */
+    public void setThrowable(Throwable throwable)
+    {
+        this.throwable_ = throwable;
+    }
+
+    /**
+     * 例外発生回数を取得する。
+     * @return 例外発生回数
+     */
+    public long getThrowTime()
+    {
+        return this.throwTime_;
+    }
+
+    /**
+     * 例外発生回数を設定する。
+     * @param throwTime 例外発生回数。
+     */
+    public void setThrowTime(long throwTime)
+    {
+        this.throwTime_ = throwTime;
+    }
+
+    /**
+     * VMのステータスを取得する。
+     * @return VMのステータス
+     */
+    public VMStatus getEndVmStatus()
     {
         return this.endVmStatus_;
     }
 
-    public VMStatus getStartVmStatus( )
+    /**
+     * VMのステータスを設定する。
+     * @return VMのステータス
+     */
+    public VMStatus getStartVmStatus()
     {
         return this.startVmStatus_;
     }
 
+    /**
+     * 終了時のVMのステータス設定する。
+     * @param endVmStatus 終了時のVMのステータス
+     */
     public void setEndVmStatus(VMStatus endVmStatus)
     {
         this.endVmStatus_ = endVmStatus;
     }
 
+    /**
+     * 開始時のVMのステータス設定する。
+     * @param startVmStatus 開始時のVMのステータス
+     */
     public void setStartVmStatus(VMStatus startVmStatus)
     {
         this.startVmStatus_ = startVmStatus;
     }
 
+    /**
+     * ユーザ時間を取得する。
+     * @return ユーザ時間
+     */
     public long getUserTime()
     {
-        return userTime_;
+        return this.userTime_;
     }
 
+    /**
+     * ユーザ時間を設定する。
+     * @param userTime ユーザ時間
+     */
     public void setUserTime(long userTime)
     {
-        userTime_ = userTime;
+        this.userTime_ = userTime;
     }
 }
