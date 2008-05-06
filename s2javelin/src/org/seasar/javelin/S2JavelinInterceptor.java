09ae290b7b0d0fc5f3c04bd15c7b12d54980db6c
eriguchi <eriguchi@eeba10ee-d406-0410-b51a-bfa74c1c70a0>
2008-05-06 04:39:44 +0000

    CheckStyle対応
    git-svn-id: https://www.seasar.org/svn/sandbox/s2javelin/trunk@397 eeba10ee-d406-0410-b51a-bfa74c1c70a0

--- s2javelin/src/org/seasar/javelin/S2JavelinInterceptor.java
+++ s2javelin/src/org/seasar/javelin/S2JavelinInterceptor.java
@@ -56,96 +56,95 @@ import org.seasar.framework.log.Logger;
  */
 public class S2JavelinInterceptor extends AbstractInterceptor
 {
-    private static final long serialVersionUID = 6661781313519708185L;
+    private static final long             serialVersionUID        = 6661781313519708185L;
 
     /**
      * メソッド呼び出し時にJavlinログの先頭に出力する識別子。
      */
-    private static final String CALL = "Call  ";
+    private static final String           CALL                    = "Call  ";
 
     /**
      * メソッド戻り時にJavlinログの先頭に出力する識別子。
      */
-    private static final String RETURN = "Return";
+    private static final String           RETURN                  = "Return";
 
     /**
      * 例外throw時にJavlinログの先頭に出力する識別子。
      */
-    private static final String THROW = "Throw ";
+    private static final String           THROW                   = "Throw ";
 
     /**
      * 例外catch時にJavlinログの先頭に出力する識別子。
      */
-    private static final String CATCH = "Catch ";
+    private static final String           CATCH                   = "Catch ";
 
-    
-    private static final int CALLER_STACKTRACE_INDEX = 3;
+    private static final int              CALLER_STACKTRACE_INDEX = 3;
 
     /**
      * 改行文字。
      */
-    private static final String NEW_LINE = System.getProperty("line.separator");
+    private static final String           NEW_LINE                =
+                                                                          System.getProperty("line.separator");
 
     /**
      * ログの区切り文字。
      */
-    private static final String DELIM = ",";
+    private static final String           DELIM                   = ",";
 
     /**
      * スレッド情報の区切り文字
      */
-    private static final String THREAD_DELIM = "@";
+    private static final String           THREAD_DELIM            = "@";
 
     /**
      * ログ出力する時刻のフォーマット。
      */
-    private static final String TIME_FORMAT_STR = "yyyy/MM/dd HH:mm:ss.SSS";
+    private static final String           TIME_FORMAT_STR         = "yyyy/MM/dd HH:mm:ss.SSS";
 
     /**
      * Javelinログに出力する時刻データ整形用フォーマッタ。
      */
-    private ThreadLocal<DateFormat> timeFormat_ = new ThreadLocal<DateFormat>()
-    {
-        protected synchronized DateFormat initialValue()
-        {
-            return new SimpleDateFormat(TIME_FORMAT_STR);
-        }
-    };
+    private ThreadLocal<DateFormat>       timeFormat_             = new ThreadLocal<DateFormat>() {
+                                                                      protected synchronized DateFormat initialValue()
+                                                                      {
+                                                                          return new SimpleDateFormat(
+                                                                                                      TIME_FORMAT_STR);
+                                                                      }
+                                                                  };
 
     /**
      * メソッドの呼び出し元オブジェクトを表す文字列。
      */
-    private ThreadLocal<String> callerLog_ = new ThreadLocal<String>()
-    {
-        protected synchronized String initialValue()
-        {
-            return "<unknown>,<unknown>,0";
-        }
-    };
+    private ThreadLocal<String>           callerLog_              = new ThreadLocal<String>() {
+                                                                      protected synchronized String initialValue()
+                                                                      {
+                                                                          return "<unknown>,<unknown>,0";
+                                                                      }
+                                                                  };
 
     /**
      * Javelinログ出力用ロガー。
      */
-    private static Logger logger_ = Logger
-            .getLogger(S2JavelinInterceptor.class);
+    private static Logger                 logger_                 =
+                                                                          Logger.getLogger(S2JavelinInterceptor.class);
 
     /**
      * 引数を出力するかどうか。
      * argsプロパティの値がセットされる。
      */
-    private boolean isLogArgs_ = true;
+    private boolean                       isLogArgs_              = true;
 
     /**
      * 戻り値を出力するかどうか。
      * returnプロパティの値がセットされる。
      */
-    private boolean isLogReturn_ = true;
+    private boolean                       isLogReturn_            = true;
 
     /**
      * スタックトレースを出力するかどうか。
      * stackTraceプロパティの値がセットされる。
      */
-    private boolean isLogStackTrace_ = false;
+    private boolean                       isLogStackTrace_        = false;
 
     /**
      * 例外発生を記録するテーブル。
@@ -153,16 +152,16 @@ public class S2JavelinInterceptor extends AbstractInterceptor
      * 例外が発生したらこのマップにその例外を登録し、
      * キャッチされた時点で削除する。
      */
-    static private Map<String, Throwable> exceptionMap__ = 
-    	Collections.synchronizedMap(new HashMap<String, Throwable>());
-    
-    private S2JavelinConfig config_ = new S2JavelinConfig();
+    static private Map<String, Throwable> exceptionMap__          =
+                                                                          Collections.synchronizedMap(new HashMap<String, Throwable>());
+
+    private S2JavelinConfig               config_                 = new S2JavelinConfig();
 
     /** 設定値を標準出力に出力したらtrue */
-    private boolean isPrintConfig_ = false;
+    private boolean                       isPrintConfig_          = false;
 
-	private boolean isInitialized_ = false;
-    
+    private boolean                       isInitialized_          = false;
+
     /**
      * Javelinログ出力用のinvokeメソッド。
      * 
@@ -181,72 +180,65 @@ public class S2JavelinInterceptor extends AbstractInterceptor
      * @return invocationを実行したときの戻り値
      * @throws Throwable invocationを実行したときに発生した例外
      */
-    public Object invoke(MethodInvocation invocation) throws Throwable
+    public Object invoke(MethodInvocation invocation)
+        throws Throwable
     {
         // 設定値を出力していなければ出力する
         synchronized (this)
         {
-            if (isPrintConfig_ == false)
+            if (this.isPrintConfig_ == false)
             {
-                isPrintConfig_ = true;
+                this.isPrintConfig_ = true;
                 printConfigValue();
             }
-            
+
             initialize();
         }
 
         // 呼び出し先情報取得。
-        String calleeClassName  = getTargetClass(invocation).getName();
+        String calleeClassName = getTargetClass(invocation).getName();
         String calleeMethodName = invocation.getMethod().getName();
-        
+
         try
         {
             // 呼び出し先情報取得。
-            String className  = calleeClassName;
+            String className = calleeClassName;
             String methodName = calleeMethodName;
 
-            StackTraceElement[] stacktrace  = null;
-            if (config_.isLogStacktrace())
+            StackTraceElement[] stacktrace = null;
+            if (this.config_.isLogStacktrace())
             {
-            	stacktrace = Thread.currentThread().getStackTrace();
+                stacktrace = Thread.currentThread().getStackTrace();
             }
-            
-            S2StatsJavelinRecorder.preProcess(className, methodName,
-                                              invocation.getArguments(), stacktrace, this.config_);
+
+            S2StatsJavelinRecorder.preProcess(className, methodName, invocation.getArguments(),
+                                              stacktrace, this.config_);
         }
-        catch(Throwable th)
+        catch (Throwable th)
         {
-        	th.printStackTrace();
+            th.printStackTrace();
         }
-        
-        String objectID   = "";
+
+        String objectID = "";
         String threadInfo = "";
-        
+
         StringBuffer methodCallBuff = new StringBuffer(256);
         // 呼び出し元情報取得。
         String currentCallerLog = "";
 
-        if (config_.isJavelinEnable())
+        if (this.config_.isJavelinEnable())
         {
-        	currentCallerLog = (String) this.callerLog_.get();
-        	
-            objectID = Integer.toHexString(System
-                    .identityHashCode(invocation.getThis()));
-            String modifiers = Modifier.toString(invocation.getMethod()
-                    .getModifiers());
+            currentCallerLog = this.callerLog_.get();
 
-            Thread currentThread   = Thread.currentThread();
-            String threadName      = currentThread.getName();
+            objectID = Integer.toHexString(System.identityHashCode(invocation.getThis()));
+            String modifiers = Modifier.toString(invocation.getMethod().getModifiers());
+
+            Thread currentThread = Thread.currentThread();
+            String threadName = currentThread.getName();
             String threadClassName = currentThread.getClass().getName();
-            String threadID        = 
-            	Integer.toHexString(System.identityHashCode(currentThread));
-            
-            threadInfo = 
-            	threadName 
-            	+ THREAD_DELIM 
-            	+ threadClassName 
-            	+ THREAD_DELIM 
-            	+ threadID;
+            String threadID = Integer.toHexString(System.identityHashCode(currentThread));
+
+            threadInfo = threadName + THREAD_DELIM + threadClassName + THREAD_DELIM + threadID;
 
             // メソッド呼び出し共通部分生成。
             methodCallBuff.append(calleeMethodName);
@@ -267,8 +259,7 @@ public class S2JavelinInterceptor extends AbstractInterceptor
             methodCallBuff.append(NEW_LINE);
 
             // Call 詳細ログ生成。
-            StringBuffer callDetailBuff = createCallDetail(methodCallBuff,
-                    invocation);
+            StringBuffer callDetailBuff = createCallDetail(methodCallBuff, invocation);
 
             // Call ログ出力。
             logger_.debug(callDetailBuff);
@@ -286,11 +277,12 @@ public class S2JavelinInterceptor extends AbstractInterceptor
         {
             S2StatsJavelinRecorder.postProcess(cause, this.config_);
 
-            if (config_.isJavelinEnable())
+            if (this.config_.isJavelinEnable())
             {
                 // Throw詳細ログ生成。
-                StringBuffer throwBuff = createThrowCatchDetail(THROW, calleeMethodName,
-                        calleeClassName, objectID, threadInfo, cause);
+                StringBuffer throwBuff =
+                        createThrowCatchDetail(THROW, calleeMethodName, calleeClassName, objectID,
+                                               threadInfo, cause);
 
                 // Throw ログ出力。
                 logger_.debug(throwBuff);
@@ -298,25 +290,26 @@ public class S2JavelinInterceptor extends AbstractInterceptor
                 //例外発生を記録する。
                 exceptionMap__.put(threadInfo, cause);
             }
-            
+
             //例外をスローし、終了する。
             throw cause;
         }
-        
-        if (config_.isJavelinEnable())
+
+        if (this.config_.isJavelinEnable())
         {
             //このスレッドで、直前に例外が発生していたかを確認する。
             //発生していてここにたどり着いたのであれば、この時点で例外が
             //catchされたということになるので、Catchログを出力する。
             boolean isExceptionThrowd = exceptionMap__.containsKey(threadInfo);
-            if(isExceptionThrowd == true) 
+            if (isExceptionThrowd == true)
             {
                 //発生していた例外オブジェクトをマップから取り出す。（かつ削除する）
-                Throwable exception = (Throwable)exceptionMap__.remove(threadInfo);
-                
+                Throwable exception = exceptionMap__.remove(threadInfo);
+
                 // Catch詳細ログ生成。
-                StringBuffer throwBuff = createThrowCatchDetail(CATCH, calleeMethodName,
-                        calleeClassName, objectID, threadInfo, exception);
+                StringBuffer throwBuff =
+                        createThrowCatchDetail(CATCH, calleeMethodName, calleeClassName, objectID,
+                                               threadInfo, exception);
 
                 // Catch ログ出力。
                 logger_.debug(throwBuff);
@@ -335,32 +328,33 @@ public class S2JavelinInterceptor extends AbstractInterceptor
         {
             S2StatsJavelinRecorder.postProcess(ret, this.config_);
         }
-        catch(Throwable th)
+        catch (Throwable th)
         {
-        	th.printStackTrace();
+            th.printStackTrace();
         }
-        
+
         //invocationを実行した際の戻り値を返す。
         return ret;
     }
 
-	private void initialize() {
-		if(isInitialized_ == false)
-		{
-		    if (this.config_.getHttpPort() != 0)
-		    {
-		    	try
-		    	{
-					Mx4JLauncher.execute(this.config_.getHttpPort());
-		    	}
-		    	catch(Exception ex)
-		    	{
-		    		ex.printStackTrace();
-		    	}
-		    }
-			isInitialized_ =  true;
-		}
-	}
+    private void initialize()
+    {
+        if (this.isInitialized_ == false)
+        {
+            if (this.config_.getHttpPort() != 0)
+            {
+                try
+                {
+                    Mx4JLauncher.execute(this.config_.getHttpPort());
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+            this.isInitialized_ = true;
+        }
+    }
 
     /**
      * メソッド呼び出し（識別子Call）の詳細なログを生成する。
@@ -369,11 +363,9 @@ public class S2JavelinInterceptor extends AbstractInterceptor
      * @param invocation メソッド呼び出しの情報
      * @return メソッド呼び出しの詳細なログ
      */
-    private StringBuffer createCallDetail(StringBuffer methodCallBuff,
-            MethodInvocation invocation)
+    private StringBuffer createCallDetail(StringBuffer methodCallBuff, MethodInvocation invocation)
     {
-        String timeStr = ((SimpleDateFormat) this.timeFormat_.get())
-                .format(new Date());
+        String timeStr = ((SimpleDateFormat)this.timeFormat_.get()).format(new Date());
         StringBuffer callDetailBuff = new StringBuffer(512);
 
         callDetailBuff.append(CALL);
@@ -431,12 +423,10 @@ public class S2JavelinInterceptor extends AbstractInterceptor
      * @param ret メソッドの戻り値
      * @return メソッドの戻りの詳細なログ
      */
-    private StringBuffer createReturnDetail(StringBuffer methodCallBuff,
-            Object ret)
+    private StringBuffer createReturnDetail(StringBuffer methodCallBuff, Object ret)
     {
         StringBuffer returnDetailBuff = new StringBuffer(512);
-        String returnTimeStr = ((SimpleDateFormat) this.timeFormat_.get())
-                .format(new Date());
+        String returnTimeStr = ((SimpleDateFormat)this.timeFormat_.get()).format(new Date());
         returnDetailBuff.append(RETURN);
         returnDetailBuff.append(DELIM);
         returnDetailBuff.append(returnTimeStr);
@@ -467,13 +457,10 @@ public class S2JavelinInterceptor extends AbstractInterceptor
      * @return 例外発生の詳細なログ
      */
     private StringBuffer createThrowCatchDetail(String id, String calleeMethodName,
-            String calleeClassName, String objectID, String threadInfo,
-            Throwable cause)
+            String calleeClassName, String objectID, String threadInfo, Throwable cause)
     {
-        String throwTimeStr = ((SimpleDateFormat) this.timeFormat_.get())
-                .format(new Date());
-        String throwableID = Integer
-                .toHexString(System.identityHashCode(cause));
+        String throwTimeStr = ((SimpleDateFormat)this.timeFormat_.get()).format(new Date());
+        String throwableID = Integer.toHexString(System.identityHashCode(cause));
         StringBuffer throwBuff = new StringBuffer(512);
         throwBuff.append(id);
         throwBuff.append(DELIM);
@@ -565,7 +552,7 @@ public class S2JavelinInterceptor extends AbstractInterceptor
             this.config_.setJavelinFileDir(javelinFileDir);
         }
     }
-    
+
     /**
      * 
      * @param httpPort
@@ -621,38 +608,37 @@ public class S2JavelinInterceptor extends AbstractInterceptor
      * 
      * @param endCalleeName
      */
-	public void setEndCalleeName(String endCalleeName)
-	{
+    public void setEndCalleeName(String endCalleeName)
+    {
         if (this.config_.isSetEndCalleeName() == false)
         {
             this.config_.setEndCalleeName(endCalleeName);
         }
-	}
+    }
 
     /**
      * 
      * @param endCallerName
      */
-	public void setRootCallerName(String endCallerName)
-	{
+    public void setRootCallerName(String endCallerName)
+    {
         if (this.config_.isSetRootCallerName() == false)
         {
             this.config_.setRootCallerName(endCallerName);
         }
-	}
+    }
 
     /**
      * 
      * @param threadModel
      */
-	public void setThreadModel(int threadModel)
-	{
+    public void setThreadModel(int threadModel)
+    {
         if (this.config_.isSetThreadModel() == false)
         {
             this.config_.setThreadModel(threadModel);
         }
-	}
-
+    }
 
     /**
      * 設定値を標準出力に出力する。
